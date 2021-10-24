e47f91f5fb7784947245888fd39e75f7d0c3b496
Mikkhael <35925706+Mikkhael@users.noreply.github.com>
2021-10-24 19:57:55 +0200

    organized and created database manager

--- index.js
+++ index.js
@@ -10,47 +10,87 @@ const PORT = process.env.PORT || 8080;
 // MYSQL
 
 const DB_CRED = require('./db_credentials');
-const connection_params = {
-    database: DB_CRED.NAME,
-    port: +DB_CRED.PORT,
-    host: DB_CRED.ADDR,
-    user: DB_CRED.USER,
-    supportBigNumbers: true,
-};
-if(DB_CRED.PASS){
-    connection_params.password = DB_CRED.PASS;
-}
-const connection = mysql.createConnection(connection_params);
-
-/**
- * 
- * @param {string[]} times 
- * @param {BigInt[]} thermometers_ids 
- * @param {number[]} values 
- */
-function insert_new_measures(times, thermometers_ids, values){
-    const NO_VALUE = -200
-    let query = `INSERT INTO measures (time, thermometer_id, value) VALUES `;
-    for(let time_i=0; time_i<times.length; time_i++){
-        const time = times[time_i];
-        for (let i = 0; i < thermometers_ids.length; i++) {
-            const thermometer_id = thermometers_ids[i];
-            const value = values[time_i*times.length + i];
-            if(value === NO_VALUE){
-                continue;
+class DBManager{
+    constructor(){
+        this.default_connection_params = {
+            database: DB_CRED.NAME,
+            port: +DB_CRED.PORT,
+            host: DB_CRED.ADDR,
+            user: DB_CRED.USER,
+            supportBigNumbers: true,
+        };
+        if(DB_CRED.PASS){
+            this.default_connection_params.password = DB_CRED.PASS;
+        }
+        this.connection = null;
+        this.last_connection_params = Object.assign({}, this.default_connection_params);
+    }
+    connect(connection_params = {}, next = function(err){}){
+        this.last_connection_params = Object.assign({}, this.default_connection_params, connection_params);
+        this.#_connect_impl(next);
+    }
+    #_connect_impl(next = function(err){}){
+        this.connection = mysql.createConnection(this.last_connection_params);
+        this.connection.connect(function (err){
+            if(err){
+                console.error('Error while connecting to MySQL server: ', err);
             }
-            query += `('${time}',${thermometer_id},${value}),`;
+            next(err);
+        });
+    }
+    reconnect(next = function(err){}){
+        if(!this.is_connected()){
+            this.#_connect_impl(next);
         }
     }
-    query = query.slice(0,-1);
-    connection.query(query,function(err){
-        if(err){
-            console.error("Error while perofming insert query: ", err);
+    is_connected(){
+        return this.connection && this.connection.authorized;
+    }
+    
+    /**
+     * 
+     * @param {Date[]} times 
+     * @param {BigInt[]} thermometers_ids 
+     * @param {number[]} values 
+     */
+    insert_new_measurements(times, thermometers_ids, values, next = function(err){}){
+        if(!this.is_connected()){
+            console.log("Connection disconnected. Reconnecting...");
+            this.reconnect((err)=>{
+                if(err){
+                    next(err);
+                    return;
+                }
+                this.insert_new_measurements(times, thermometers_ids, values, next);
+            });
             return;
         }
-        console.log("Insert executed");
-    });
-}
+        const NO_VALUE = -200;
+        let query = `INSERT INTO measurements (time, thermometer_id, value) VALUES `;
+        for(let time_i=0; time_i<times.length; time_i++){
+            const time = times[time_i];
+            for (let i = 0; i < thermometers_ids.length; i++) {
+                const thermometer_id = thermometers_ids[i];
+                const value = values[time_i*times.length + i];
+                if(value === NO_VALUE){
+                    continue;
+                }
+                query += `(${mysql.escape(time)},${thermometer_id},${value}),`;
+            }
+        }
+        query = query.slice(0,-1);
+        this.connection.query(query,(err) => {
+            if(err){
+                console.error("Error while perofming insert query: ", err);
+            }
+            next(err);
+        });
+    }
+    
+};
+
+const db = new DBManager();
+db.connect();
 
 // WEB INTERFACE
 
@@ -71,9 +111,9 @@ Post New Tmeperatures Request Body Format:
     - uint8  request_type (10)
     - uint32 device_id
     - uint32 thermometers_count
-    - uint32 measures_count
+    - uint32 measurements_count
     - uint64 thermometer_ids[thermometers_count]
-    - int16 measures[measures_count][thermometers_count]
+    - int16 measurements[measurements_count][thermometers_count]
 */
 
 const RequestTypes = {
@@ -107,13 +147,13 @@ function handlePostNewTemperatures(ws, data){
     }
     const device_id = data.readUInt32LE(0);
     const thermometers_count = data.readUInt32LE(4);
-    const measures_count = data.readUInt32LE(8);
-    const expected_request_length = 12 + thermometers_count * 8 + thermometers_count * measures_count * 2;
+    const measurements_count = data.readUInt32LE(8);
+    const expected_request_length = 12 + thermometers_count * 8 + thermometers_count * measurements_count * 2;
     
     console.log(`Received Post New Temperatures Request 1:`, {
         device_id,
         thermometers_count,
-        measures_count
+        measurements_count
     });
     
     if(data.length !== expected_request_length){
@@ -122,21 +162,20 @@ function handlePostNewTemperatures(ws, data){
     }
     
     const thermometers_ids = [];
-    const measures = [];
+    const measurements = [];
     for(let i=0; i<thermometers_count; i++){
         thermometers_ids.push(data.readBigUInt64LE(12+i*8));
     }
-    for(let i=0; i<measures_count*thermometers_count; i++){
-        measures.push(data.readInt16LE(12+thermometers_count*8+i*2));
+    for(let i=0; i<measurements_count*thermometers_count; i++){
+        measurements.push(data.readInt16LE(12+thermometers_count*8+i*2));
     }
     console.log(`Received Post New Temperatures Request 2:`, {
         thermometers_ids,
-        measures
+        measurements
     });
     
-    console.log('Inserting into databse');
-    const times = new Array(measures_count).fill(new Date().toISOString());
-    insert_new_measures(times, thermometers_ids, measures);
+    const times = new Array(measurements_count).fill(new Date());
+    db.insert_new_measurements(times, thermometers_ids, measurements);
 }
 function invalidPostNewTemperatureRequest(ws, data, reason){
     console.log(`Invalid Post New Request Temperature Request: `, reason);
